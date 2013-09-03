4b553737d2a7edad590dcf1f5747a5cb7f3315ca
oryanm <oryan.mat@gmail.com>
2013-09-03 19:03:12 +0300

    replace JPanel with Container

--- src/net/oryanmat/gui/util/layout/BorderLayoutBuilder.java
+++ src/net/oryanmat/gui/util/layout/BorderLayoutBuilder.java
@@ -1,43 +1,42 @@
 package net.oryanmat.gui.util.layout;
 
-import javax.swing.*;
 import java.awt.*;
 
 @SuppressWarnings("UnusedDeclaration")
-public class BorderLayoutBuilder<T extends JPanel> {
-	private T panel;
+public class BorderLayoutBuilder<T extends Container> {
+	private T container;
 
-	protected BorderLayoutBuilder(T panel, int horizontalGap, int verticalGap) {
-		this.panel = panel;
-		this.panel.setLayout(new BorderLayout(horizontalGap, verticalGap));
+	protected BorderLayoutBuilder(T container, int horizontalGap, int verticalGap) {
+		this.container = container;
+		this.container.setLayout(new BorderLayout(horizontalGap, verticalGap));
 	}
 
-	public T getPanel() {
-		return this.panel;
+	public T getContainer() {
+		return this.container;
 	}
 
 	public BorderLayoutBuilder<T> setCenter(Component component) {
-		this.panel.add(component, BorderLayout.CENTER);
+		this.container.add(component, BorderLayout.CENTER);
 		return this;
 	}
 
 	public BorderLayoutBuilder<T> setNorth(Component component) {
-		this.panel.add(component, BorderLayout.NORTH);
+		this.container.add(component, BorderLayout.NORTH);
 		return this;
 	}
 
 	public BorderLayoutBuilder<T> setSouth(Component component) {
-		this.panel.add(component, BorderLayout.SOUTH);
+		this.container.add(component, BorderLayout.SOUTH);
 		return this;
 	}
 
 	public BorderLayoutBuilder<T> setEast(Component component) {
-		this.panel.add(component, BorderLayout.EAST);
+		this.container.add(component, BorderLayout.EAST);
 		return this;
 	}
 
 	public BorderLayoutBuilder<T> setWest(Component component) {
-		this.panel.add(component, BorderLayout.WEST);
+		this.container.add(component, BorderLayout.WEST);
 		return this;
 	}
 }
