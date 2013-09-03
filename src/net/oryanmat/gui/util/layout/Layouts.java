75747b9fbc41e4a9cbb25a3d979dfd75b580aa91
oryanm <oryan.mat@gmail.com>
2013-09-03 19:09:27 +0300

    replace JPanel with Container

--- src/net/oryanmat/gui/util/layout/Layouts.java
+++ src/net/oryanmat/gui/util/layout/Layouts.java
@@ -11,119 +11,119 @@ public class Layouts {
 		}
 
 		public static BorderLayoutBuilder<JPanel> buildJPanel(int horizontalGap, int verticalGap) {
-			return buildPanel(new JPanel(), horizontalGap, verticalGap);
+			return buildContainer(new JPanel(), horizontalGap, verticalGap);
 		}
 
-		public static <T extends JPanel> BorderLayoutBuilder<T> buildPanel(T panel) {
-			return buildPanel(panel, 0, 0);
+		public static <T extends Container> BorderLayoutBuilder<T> buildContainer(T container) {
+			return buildContainer(container, 0, 0);
 		}
 
-		public static <T extends JPanel> BorderLayoutBuilder<T> buildPanel(T panel, int horizontalGap, int verticalGap) {
-			return new BorderLayoutBuilder<T>(panel, horizontalGap, verticalGap);
+		public static <T extends Container> BorderLayoutBuilder<T> buildContainer(T container, int horizontalGap, int verticalGap) {
+			return new BorderLayoutBuilder<T>(container, horizontalGap, verticalGap);
 		}
 	}
 
 	public static class Flow {
 		public static JPanel getJPanel(Component... components) {
-			return getPanel(new JPanel(), components);
+			return getContainer(new JPanel(), components);
 		}
 
 		public static JPanel getJPanel(int align, Component... components) {
-			return getPanel(new JPanel(), align, components);
+			return getContainer(new JPanel(), align, components);
 		}
 
 		public static JPanel getJPanel(int align, int horizontalGap, int verticalGap, Component... components) {
-			return getPanel(new JPanel(), align, horizontalGap, verticalGap, components);
+			return getContainer(new JPanel(), align, horizontalGap, verticalGap, components);
 		}
 
-		public static <T extends JPanel> T getPanel(T panel, Component... components) {
-			return getPanel(panel, FlowLayout.CENTER, components);
+		public static <T extends Container> T getContainer(T container, Component... components) {
+			return getContainer(container, FlowLayout.CENTER, components);
 		}
 
-		public static <T extends JPanel> T getPanel(T panel, int align, Component... components) {
-			return getPanel(panel, align, 5, 5, components);
+		public static <T extends Container> T getContainer(T container, int align, Component... components) {
+			return getContainer(container, align, 5, 5, components);
 		}
 
-		public static <T extends JPanel> T getPanel(T panel, int align, int horizontalGap, int verticalGap, Component... components) {
-			return buildPanel(panel, align, horizontalGap, verticalGap, components).getPanel();
+		public static <T extends Container> T getContainer(T container, int align, int horizontalGap, int verticalGap, Component... components) {
+			return buildContainer(container, align, horizontalGap, verticalGap, components).getContainer();
 		}
 
-		public static <T extends JPanel> FlowLayoutBuilder<T> buildPanel(T panel, Component... components) {
-			return buildPanel(panel, FlowLayout.CENTER, components);
+		public static <T extends Container> FlowLayoutBuilder<T> buildContainer(T container, Component... components) {
+			return buildContainer(container, FlowLayout.CENTER, components);
 		}
 
-		public static <T extends JPanel> FlowLayoutBuilder<T> buildPanel(T panel, int align, Component... components) {
-			return buildPanel(panel, align, 5, 5, components);
+		public static <T extends Container> FlowLayoutBuilder<T> buildContainer(T container, int align, Component... components) {
+			return buildContainer(container, align, 5, 5, components);
 		}
 
-		public static <T extends JPanel> FlowLayoutBuilder<T> buildPanel(T panel, int align, int horizontalGap, int verticalGap, Component... components) {
-			return new FlowLayoutBuilder<T>(panel, align, horizontalGap, verticalGap).add(components);
+		public static <T extends Container> FlowLayoutBuilder<T> buildContainer(T container, int align, int horizontalGap, int verticalGap, Component... components) {
+			return new FlowLayoutBuilder<T>(container, align, horizontalGap, verticalGap).add(components);
 		}
 	}
 
 	public static class Box {
 		public static JPanel getHorizontalJPanel(int gap, Component... components) {
-			return getHorizontalPanel(new JPanel(), gap, components);
+			return getHorizontalContainer(new JPanel(), gap, components);
 		}
 
 		public static JPanel getVerticalJPanel(int gap, Component... components) {
-			return getVerticalPanel(new JPanel(), gap, components);
+			return getVerticalContainer(new JPanel(), gap, components);
 		}
 
 		public static JPanel getHorizontalJPanel(Component... components) {
-			return getHorizontalPanel(new JPanel(), components);
+			return getHorizontalContainer(new JPanel(), components);
 		}
 
 		public static JPanel getVerticalJPanel(Component... components) {
-			return getVerticalPanel(new JPanel(), components);
+			return getVerticalContainer(new JPanel(), components);
 		}
 
-		public static <T extends JPanel> T getHorizontalPanel(T panel, Component... components) {
-			return getPanel(panel, BoxLayout.LINE_AXIS, components);
+		public static <T extends Container> T getHorizontalContainer(T container, Component... components) {
+			return getContainer(container, BoxLayout.LINE_AXIS, components);
 		}
 
-		public static <T extends JPanel> T getVerticalPanel(T panel, Component... components) {
-			return getPanel(panel, BoxLayout.PAGE_AXIS, components);
+		public static <T extends Container> T getVerticalContainer(T container, Component... components) {
+			return getContainer(container, BoxLayout.PAGE_AXIS, components);
 		}
 
-		public static <T extends JPanel> T getHorizontalPanel(T panel, int gap, Component... components) {
-			return getPanel(panel, BoxLayout.LINE_AXIS, gap, components);
+		public static <T extends Container> T getHorizontalContainer(T container, int gap, Component... components) {
+			return getContainer(container, BoxLayout.LINE_AXIS, gap, components);
 		}
 
-		public static <T extends JPanel> T getVerticalPanel(T panel, int gap, Component... components) {
-			return getPanel(panel, BoxLayout.PAGE_AXIS, gap, components);
+		public static <T extends Container> T getVerticalContainer(T container, int gap, Component... components) {
+			return getContainer(container, BoxLayout.PAGE_AXIS, gap, components);
 		}
 
-		private static <T extends JPanel> T getPanel(T panel, int axis, Component... components) {
-			return buildPanel(panel, axis, components).getPanel();
+		private static <T extends Container> T getContainer(T container, int axis, Component... components) {
+			return buildContainer(container, axis, components).getContainer();
 		}
 
-		private static <T extends JPanel> T getPanel(T panel, int axis, int gap, Component... components) {
-			return buildPanel(panel, axis, gap, components).getPanel();
+		private static <T extends Container> T getContainer(T container, int axis, int gap, Component... components) {
+			return buildContainer(container, axis, gap, components).getContainer();
 		}
 
-		public static <T extends JPanel> BoxLayoutBuilder<T> buildHorizontalPanel(T panel, Component... components) {
-			return buildPanel(panel, BoxLayout.LINE_AXIS, components);
+		public static <T extends Container> BoxLayoutBuilder<T> buildHorizontalContainer(T container, Component... components) {
+			return buildContainer(container, BoxLayout.LINE_AXIS, components);
 		}
 
-		public static <T extends JPanel> BoxLayoutBuilder<T> buildVerticalPanel(T panel, Component... components) {
-			return buildPanel(panel, BoxLayout.PAGE_AXIS, components);
+		public static <T extends Container> BoxLayoutBuilder<T> buildVerticalContainer(T container, Component... components) {
+			return buildContainer(container, BoxLayout.PAGE_AXIS, components);
 		}
 
-		public static <T extends JPanel> BoxLayoutBuilder<T> buildHorizontalPanel(T panel, int gap, Component... components) {
-			return buildPanel(panel, BoxLayout.LINE_AXIS, gap, components);
+		public static <T extends Container> BoxLayoutBuilder<T> buildHorizontalContainer(T container, int gap, Component... components) {
+			return buildContainer(container, BoxLayout.LINE_AXIS, gap, components);
 		}
 
-		public static <T extends JPanel> BoxLayoutBuilder<T> buildVerticalPanel(T panel, int gap, Component... components) {
-			return buildPanel(panel, BoxLayout.PAGE_AXIS, gap, components);
+		public static <T extends Container> BoxLayoutBuilder<T> buildVerticalContainer(T container, int gap, Component... components) {
+			return buildContainer(container, BoxLayout.PAGE_AXIS, gap, components);
 		}
 
-		private static <T extends JPanel> BoxLayoutBuilder<T> buildPanel(T panel, int axis, Component... components) {
-			return new BoxLayoutBuilder<T>(panel, axis).add(components);
+		private static <T extends Container> BoxLayoutBuilder<T> buildContainer(T container, int axis, Component... components) {
+			return new BoxLayoutBuilder<T>(container, axis).add(components);
 		}
 
-		private static <T extends JPanel> BoxLayoutBuilder<T> buildPanel(T panel, int axis, int gap, Component... components) {
-			return new BoxLayoutBuilder<T>(panel, axis, gap).add(components);
+		private static <T extends Container> BoxLayoutBuilder<T> buildContainer(T container, int axis, int gap, Component... components) {
+			return new BoxLayoutBuilder<T>(container, axis, gap).add(components);
 		}
 	}
 }
