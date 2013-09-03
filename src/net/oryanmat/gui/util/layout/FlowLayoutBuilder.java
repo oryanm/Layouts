36159c7c875252389bc0bad7213aa85b95de9a59
oryanm <oryan.mat@gmail.com>
2013-09-03 19:03:31 +0300

    replace JPanel with Container

--- src/net/oryanmat/gui/util/layout/FlowLayoutBuilder.java
+++ src/net/oryanmat/gui/util/layout/FlowLayoutBuilder.java
@@ -1,25 +1,24 @@
 package net.oryanmat.gui.util.layout;
 
-import javax.swing.*;
 import java.awt.*;
 
 @SuppressWarnings("UnusedDeclaration")
-public class FlowLayoutBuilder<T extends JPanel> {
-	private T panel;
+public class FlowLayoutBuilder<T extends Container> {
+	private T container;
 
 	@SuppressWarnings("MagicConstant")
-	protected FlowLayoutBuilder(T panel, int align, int horizontalGap, int verticalGap) {
-		this.panel = panel;
-		this.panel.setLayout(new FlowLayout(align, horizontalGap, verticalGap));
+	protected FlowLayoutBuilder(T container, int align, int horizontalGap, int verticalGap) {
+		this.container = container;
+		this.container.setLayout(new FlowLayout(align, horizontalGap, verticalGap));
 	}
 
-	public T getPanel() {
-		return this.panel;
+	public T getContainer() {
+		return this.container;
 	}
 
 	public FlowLayoutBuilder<T> add(Component... components) {
 		for (Component component : components) {
-			this.panel.add(component);
+			this.container.add(component);
 		}
 
 		return this;
