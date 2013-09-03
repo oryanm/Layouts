6d56b891a5312ea5aef58ece348804480da670d9
oryanm <oryan.mat@gmail.com>
2013-09-03 21:15:42 +0300

    added AbstractBuilder

--- src/net/oryanmat/gui/util/layout/FlowLayoutBuilder.java
+++ src/net/oryanmat/gui/util/layout/FlowLayoutBuilder.java
@@ -3,20 +3,13 @@ package net.oryanmat.gui.util.layout;
 import java.awt.*;
 
 @SuppressWarnings("UnusedDeclaration")
-public class FlowLayoutBuilder<T extends Container> {
-	private T container;
-
+public class FlowLayoutBuilder<C extends Container> extends AbstractBuilder<C, FlowLayout>{
 	@SuppressWarnings("MagicConstant")
-	protected FlowLayoutBuilder(T container, int align, int horizontalGap, int verticalGap) {
-		this.container = container;
-		this.container.setLayout(new FlowLayout(align, horizontalGap, verticalGap));
+	protected FlowLayoutBuilder(C container, int align, int horizontalGap, int verticalGap) {
+		super(container, new FlowLayout(align, horizontalGap, verticalGap));
 	}
 
-	public T getContainer() {
-		return this.container;
-	}
-
-	public FlowLayoutBuilder<T> add(Component... components) {
+	public FlowLayoutBuilder<C> add(Component... components) {
 		for (Component component : components) {
 			this.container.add(component);
 		}
